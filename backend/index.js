5a415e8d46fd04e8d7b312e326f4ffc1a2b8e849
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-06-15 00:22:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-    origin : ["https://blogging-site-mern-api.vercel.app/"],
+    origin : "https://blogging-site-mern-api.vercel.app/",
     methods: ["POST", "GET", "DELETE"],
     credentials:true
 }));
