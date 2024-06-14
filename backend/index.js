65cde8d0f3b33c1a13862872c4eb142fb0baccd4
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-06-14 23:47:02 +0530

    cors-related

--- backend/index.js
+++ backend/index.js
@@ -11,19 +11,19 @@ const userRoutes = require('./routes/userRoutes');
 const app = express() 
 
 app.use(express.json());
-app.use(cors(
-    {
-       origin:["https://blogging-site-mern-api-git-master-vg3627s-projects.vercel.app"],
-       methods: ['POST', 'GET', 'DELETE'],
-       credentials: true
-    }
+app.use(cors( async(req,res) => {
     
-));
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust this to your frontend URL in production
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+}));
 app.use('/api/blogs', blogRoutes)
 
 const dburl = process.env.MONGO_URI ;
 mongoose.set('strictQuery',true) ;
-const port = process.env.PORT || 3069;
+const port = process.env.PORT || 8080;
 mongoose.connect(dburl) 
 .then((result) => app.listen(port)) // we want to listen for  after server is connected to mongodb
 .catch((error) => console.log(error)) ;
